d2c8a3cb64d97e26a8009261e9ce2092905450e5
MorcaDev <morca225@gmail.com>
2021-12-17 18:31:08 -0500

    basic algorithm of frequentButton

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 console.log('Hi dude')
 
 ///////////////////////
+
+let numberOfViews   = document.getElementById("numberPages")
+let price           = document.getElementById("priceNumbers")
+let frequent        = document.getElementById("frequentlyOption")
+let frequencyButton = document.getElementById("button")
+let frequentCircle  = document.getElementById("buttonCircle")
+let discountNumber  = document.getElementById("percentNumber")
+
+
+let changeFrequent = (ev) =>{
+
+    //change button styles
+    frequencyButton.classList.toggle("activate")
+
+    //change value of frequent "text"
+    let currentFrequent = frequent.textContent 
+
+    if( currentFrequent === "/ month"){
+        
+        frequent.textContent = "/ year"
+
+    }else if ( currentFrequent === "/ year"){
+
+        frequent.textContent = "/ month"
+   
+    }
+}
+
+frequencyButton.addEventListener("click",changeFrequent)
