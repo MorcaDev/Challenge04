23852b75533d118bb6e761a099600fc97f267e6c
MorcaDev <morca225@gmail.com>
2021-12-20 09:27:53 -0500

    changes from input

--- script.js
+++ script.js
@@ -1,11 +1,115 @@
-let numberOfViews   = document.getElementById("numberPages")
-let price           = document.getElementById("priceNumbers")
-let frequent        = document.getElementById("frequentlyOption")
-let frequencyButton = document.getElementById("button")
-let frequentCircle  = document.getElementById("buttonCircle")
-let discountNumber  = document.getElementById("percentNumber")
+///////////////////////////////
+/*           NODES           */
+///////////////////////////////
 
-///////////////////////
+const frequent        = document.getElementById("frequentlyOption");
+const frequencyButton = document.getElementById("button");
+const frequentCircle  = document.getElementById("buttonCircle");
+const bar             = document.getElementById("barItself");
+const numberOfPages   = document.getElementById("numberPages");
+const price           = document.getElementById("priceNumbers");
+const discountNumber  = document.getElementById("percentNumber");
+let isYearly          = undefined;
+
+///////////////////////////////
+/*       INFORMATION         */
+///////////////////////////////
+
+const changes  = [
+    {
+        value : 1,
+        pages : "10K",
+        priceM : "$8.00",
+        priceY : "$6.00",
+    },
+    {
+        value : 2,
+        pages : "50K",
+        priceM : "$12.00",
+        priceY : "$9.00",
+    },
+    {
+        value : 3,
+        pages : "100K",
+        priceM : "$16.00",
+        priceY : "$12.00",
+    },
+    {
+        value : 4,
+        pages : "500K",
+        priceM : "$24.00",
+        priceY : "$18.00",
+    },
+    {
+        value : 5,
+        pages : "1M",
+        priceM : "$36.00",
+        priceY : "$27.00",
+    }
+]
+
+///////////////////////////////
+/*      FREQUENT BUTTON      */
+///////////////////////////////
+
+let changeFrequent = (ev) =>{
+
+    //change button styles
+    frequencyButton.classList.toggle("activate")
+
+    //change value of frequent "text"
+    let currentFrequent = frequent.textContent 
+
+    if( currentFrequent === "/ month"){
+        
+        frequent.textContent = "/ year"
+        isYearly = true
+
+    }else if ( currentFrequent === "/ year"){
+
+        frequent.textContent = "/ month"
+        isYearly = false
+   
+    }
+
+    getValue()
+
+}
+
+frequencyButton.addEventListener("click",changeFrequent)
+
+///////////////////////////////
+/*     CHANGES FROM INPUT    */
+///////////////////////////////
+
+let getValue = (ev) =>{
+
+    let barValue        = bar.value
+    let positionInArray = barValue - 1
+
+    let Currentpages   = changes[positionInArray].pages 
+    let Currentprice    = undefined
+
+    if (isYearly) {
+
+        Currentprice  = changes[positionInArray].priceY
+
+    }else{
+
+        Currentprice  = changes[positionInArray].priceM
+
+    }
+
+    numberOfPages.textContent   = Currentpages
+    price.textContent           = Currentprice
+
+}
+
+bar.addEventListener("input",getValue)
+
+///////////////////////////////
+/*      DISCOUNT NUMBER      */
+///////////////////////////////
 
 let changeDiscountMessage = () =>{
 
@@ -20,6 +124,7 @@ let changeDiscountMessage = () =>{
 
         discountNumber.textContent = "-25%"
         discountNumber.classList.remove("discount")
+
     }
 
 }
@@ -27,26 +132,3 @@ let changeDiscountMessage = () =>{
 changeDiscountMessage()
 
 window.addEventListener("resize",changeDiscountMessage)
-
-///////////////////////
-
-let changeFrequent = (ev) =>{
-
-    //change button styles
-    frequencyButton.classList.toggle("activate")
-
-    //change value of frequent "text"
-    let currentFrequent = frequent.textContent 
-
-    if( currentFrequent === "/ month"){
-        
-        frequent.textContent = "/ year"
-
-    }else if ( currentFrequent === "/ year"){
-
-        frequent.textContent = "/ month"
-   
-    }
-}
-
-frequencyButton.addEventListener("click",changeFrequent)
